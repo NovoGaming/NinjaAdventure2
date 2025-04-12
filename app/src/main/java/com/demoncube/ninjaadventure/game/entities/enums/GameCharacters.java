b7cd6250b8ce2fd9d145aa88ca13ecaf08993137
NovoGaming <134286965+NovoGaming@users.noreply.github.com>
2025-04-12 13:52:33 +0200

    rename of graphic files

--- app/src/main/java/com/demoncube/ninjaadventure/game/entities/enums/GameCharacters.java
+++ app/src/main/java/com/demoncube/ninjaadventure/game/entities/enums/GameCharacters.java
@@ -18,11 +18,11 @@ public enum GameCharacters implements BitmapMethods {
     NINJA_BLUE(R.drawable.charactersheet_ninja_blue_spritesheet),
     NINJA_BLACK(R.drawable.charactersheet_ninja_black_spritesheet),
     SKELETON(R.drawable.charactersheet_skeleton_spritesheet),
-    VILLAGER1(R.drawable.charactersheet_villiger_1_spritesheet),
-    VILLAGER2(R.drawable.charactersheet_villiger_2_spritesheet),
-    VILLAGER3(R.drawable.charactersheet_villiger_3_spritesheet),
-    VILLAGER4(R.drawable.charactersheet_villiger_4_spritesheet),
-    VILLAGER5(R.drawable.charactersheet_villiger_5_spritesheet);
+    VILLAGER1(R.drawable.charactersheet_villager_1_spritesheet),
+    VILLAGER2(R.drawable.charactersheet_villager_2_spritesheet),
+    VILLAGER3(R.drawable.charactersheet_villager_3_spritesheet),
+    VILLAGER4(R.drawable.charactersheet_villager_4_spritesheet),
+    VILLAGER5(R.drawable.charactersheet_villager_5_spritesheet);
 
     private final Bitmap spriteSheet;
     private Bitmap[][] sprites = new Bitmap[7][4];
